9835f582f7e04a189b2a6924b46468aed4b1113a
Kary Ma <karyma101@hotmail.com>
2021-06-05 00:09:01 -0700

    Add simple styling for home page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,24 @@
 import React from "react"
+import styled from "styled-components"
+import SearchBar from "../components/SearchBar"
+import backgroundImage from "../assets/home.jpg"
+
+const Container = styled.div`
+  background: #ffffff url(${backgroundImage}) no-repeat center center;
+  background-size: cover;
+  height: 100vh;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
 
 const HomePage = () => {
-  return <h1>HomePage</h1>
+  return (
+    <Container>
+      <SearchBar />
+    </Container>
+  )
 }
 
 export default HomePage
