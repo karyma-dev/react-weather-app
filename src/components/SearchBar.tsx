0615393892210cd9d92a6d47f690dee40a3db984
Kary Ma <karyma101@hotmail.com>
2021-06-05 21:11:39 -0700

    Create logic for SearchBar component

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,7 +1,32 @@
 import React from "react"
+import styled from "styled-components"
+import SearchIcon from "../assets/icons/SearchIcon"
+
+const Form = styled.form`
+  padding: 5px 0;
+  border-bottom: 2px solid white;
+`
+
+const Input = styled.input`
+  background-color: transparent;
+  border: none;
+  outline: none;
+  font-size: 20px;
+  color: white;
+`
 
 const SearchBar = () => {
-  return <h1>Search Bar</h1>
+  const onSubmit = (e) => {
+    e.preventDefault()
+    console.log(e)
+  }
+
+  return (
+    <Form onSubmit={onSubmit}>
+      <Input placeholder="Enter your city" />
+      <SearchIcon />
+    </Form>
+  )
 }
 
 export default SearchBar
