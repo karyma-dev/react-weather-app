9f8609c34c83c49646d93ae49057f2694874ca12
Kary Ma <karyma101@hotmail.com>
2021-06-15 20:54:07 -0700

    Add basic styling for main component

--- src/components/Weather/index.tsx
+++ src/components/Weather/index.tsx
@@ -1,17 +1,32 @@
 import React, { useState, useEffect } from 'react'
+import styled from 'styled-components'
 import DailyForecast from './DailyForecast'
 import HourlyForecast from './HourlyForecast'
 
+const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+`
+
 const Weather = ({ currentWeather, dailyWeather }: any) => {
-    const { name, main, weather, wind } = currentWeather
-    const { temp, temp_max, temp_min, humidity } = main
+    const { name, main, weather } = currentWeather
+    const { temp, temp_max, temp_min } = main
     const { hourly, daily } = dailyWeather
 
     return (
-        <>
+        <Container>
+            <div>
+                <h1>{name}</h1>
+                <p>{weather[0].description}</p>
+                <div>
+                    <p>{Math.round(temp_min)}</p>
+                    <p>{Math.round(temp)} &deg;C</p>
+                    <p>{Math.round(temp_max)}</p>
+                </div>
+            </div>
             <HourlyForecast hourlyForecast={hourly} />
             <DailyForecast dailyForecast={daily} />
-        </>
+        </Container>
     )
 }
 
