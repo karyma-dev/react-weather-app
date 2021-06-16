36f8af3ea5f461096d5bc764e0441c74449475d0
Kary Ma <karyma101@hotmail.com>
2021-06-15 23:15:58 -0700

    Add basic styling for DailyForecast component

--- src/components/Weather/DailyForecast.tsx
+++ src/components/Weather/DailyForecast.tsx
@@ -1,21 +1,36 @@
 import React, { FC } from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div``
+const Days = styled.ul`
+    list-style: none;
+    display: flex;
+    justify-content: space-evenly;
+    margin: 20px 0;
+`
+
+const Info = styled.li`
+    font-weight: 300;
+`
 
 const DailyForecast: FC<Props> = ({ dailyForecast }) => {
     const daysOfTheWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
 
     const mapDailyForecast = dailyForecast.slice(1).map((forecast, i) => {
-        const date = new Date(forecast.dt * 1000)
+        const { dt, weather, temp } = forecast
+
+        const date = new Date(dt * 1000)
 
         return (
-            <ul key={i}>
-                <li>{daysOfTheWeek[date.getDay()]}</li>
-                <li>{forecast.weather[0].main}</li>
-                <li>{forecast.temp.day} &deg;C</li>
-            </ul>
+            <Days key={i}>
+                <Info>{daysOfTheWeek[date.getDay()]}</Info>
+                <Info>{weather[0].main}</Info>
+                <Info>{Math.round(temp.day)} &deg;C</Info>
+            </Days>
         )
     })
 
-    return <div>{mapDailyForecast}</div>
+    return <Container>{mapDailyForecast}</Container>
 }
 
 type Props = {
